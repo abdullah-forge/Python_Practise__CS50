dbe7895a5169f694d31264e22790242cbd6e6d72
Muhammad Abdullah <whello544@gmail.com>
2026-06-20 19:44:11 +0500

    Update Cs50P.py
    doing code without AI

--- Cs50P.py
+++ Cs50P.py
@@ -13,3 +13,42 @@ def Format_profile(data):
     return f"Name : {Name}, height : {clean_height}"
 
 main()
+
+# cover topics are _, match, conditionals (if,elif,else, or and not )
+
+def main():
+    data = input("Enter Core ID and Temperature: ")
+    output = check_Reactor(data)
+    print(output)
+def check_Reactor(data):
+    parts = data.split(",")
+    core_id = parts[0]
+    temp = parts[1]
+    core_id = core_id.strip().upper()
+    temp = temp.strip()
+    temp = round(float(temp), 1)
+    ...
+    if temp <= 50.0:
+        status = 'safe'
+        state = "Safe"
+    elif temp > 50.0 and temp <= 90.0:
+        status = "Fans turn ON"
+        state = "Warning"
+    else:
+        status = "Shut down"
+        state = "Critical"
+    ...    
+    match temp:
+        case temp if temp <=  50.0:
+            status = 'safe'
+            state = "Safe"
+        case temp if temp >50.0 and temp <= 90.00:
+            status = "Fans turn ON"
+            state = "Warning"
+        case _:
+            status = "Shut down"
+            state = "Critical"
+    return f"{state}, {core_id} Temp: {temp}C -> {status}"
+
+
+main()
