255f930705b12eb2a01b6a02371905b5a08dbdd4
Muhammad Abdullah <whello544@gmail.com>
2026-06-22 15:24:58 +0500

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -10,3 +10,21 @@ print("Winner, name is ", sel)
 
 #for arg in sys.argv[1:]:
 #    print("hello, my name is ", arg)
+
+#Practice challenge 2
+
+import json
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit()
+response = requests.get(f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}")
+#requests.get("https://jsonplaceholder.typicode.com/users/" + sys.argv[1])
+print(json.dumps(response.json(), indent=4))
+
+o = response.json()
+e_name = o["name"]
+e_city = o["address"]["city"]
+
+print(f"Name : {e_name}, City : {e_city}")
