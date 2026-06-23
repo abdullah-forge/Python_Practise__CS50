5855e740586befdfdbdd67b46293160b119cefd5
Muhammad Abdullah <whello544@gmail.com>
2026-06-23 15:16:26 +0500

    Implement CSV reading and temperature sorting
    Added functionality to read from a CSV file and print sorted sensor temperature data.

---
+++ FileIO.py
@@ -0,0 +1,28 @@
+# Day4 topic covered are, file i/o ,txt, lis, csv, pillow, read, open , sorted , with, lambda 
+
+keyword = "ERROR"
+match = []
+with open ("serverlog.txt") as file:
+    for line in file:
+        status,desp = line.rstrip().split(":")
+        print(f"{status} : {desp}")
+        if keyword in line:
+            match.append(line)
+
+
+with open ("criticalError.txt" , "a") as file:
+    file.writelines(match)
+    
+# csv file 
+
+import csv 
+analytics = []
+
+with open ("sensor.csv" , "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        analytics.append({"Sensor_ID" : row["Sensor_ID"], "Temperature" : row["Temperature"]})
+
+for analytic in sorted(analytics, key = lambda analytic:int(analytic["Temperature"]), reverse=True):
+    print(f"{analytic['Sensor_ID']} temperature is {analytic['Temperature']}")
+    
