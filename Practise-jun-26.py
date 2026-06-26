5d6210a935c09532d5ee42c95eaa000b84ac0c46
Muhammad Abdullah <whello544@gmail.com>
2026-06-26 20:59:03 +0500

    Implement Box class for weight addition
    Added a Box class to calculate total weight of two boxes.

--- Practise-jun-26.py
+++ Practise-jun-26.py
@@ -6,3 +6,18 @@ text = "My emails are abdullah@gmail.com and admin@comsats.edu.pk, please reply 
 pattern = r"[a-z]+@[\w.]+"
 mail = re.findall(pattern,text)
 print(mail)
+
+# challenge 2 
+
+class Box:
+    def __init__(self, weight):
+        self.weight = weight
+    def __add__(self, other):
+        return int(self.weight + other.weight)
+    
+
+box1 = Box(10.5)
+box2 = Box(20.3)
+
+total_weight = box1 + box2
+print(f"Total Weight: {total_weight}")
